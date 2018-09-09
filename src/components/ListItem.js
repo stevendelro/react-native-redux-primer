73daea30a8b27466ac47e28482da68ab793c1c4a
Steven Del Rosario <steven@Stevens-MacBook-Pro.local>
2018-09-08 18:28:40 -0700

    Added Spring animation and adjusted some padding on the description text

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
-  
-renderDescription() {
-  const { id, title, description } = this.props.library.item;
-
-  if (id === this.props.selectedLibraryId) {
-    return (
-      <Text>{description}</Text>
-    );
+  componentDidUpdate() {
+    LayoutAnimation.spring();
   }
-}
 
+  renderDescription() {
+    const { description } = this.props.library.item;
+    if (this.props.expanded) {
+      return (
+        <CardSection>
+          <Text style={styles.descriptionStyle}>{description}</Text>
+        </CardSection>
+      );
+    }
+  }
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library.item;
@@ -37,11 +46,16 @@ const styles = StyleSheet.create({
   titleStyle: {
     fontSize: 18,
     paddingLeft: 15
+  },
+  descriptionStyle: {
+    paddingLeft: 18,
+    paddingRight: 18
   }
 });
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.item.id;
+  return { expanded };
 };
 
 export default connect(
